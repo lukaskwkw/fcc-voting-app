26d8a938ecee6c4aec6b036c1453bd6785a9703b
lukaskwkw <lukaskwkw@gmail.com>
2016-08-31 06:00:52 +0200

    New test added to signup tests

--- app/tests/server/signup.spec.js
+++ app/tests/server/signup.spec.js
@@ -14,6 +14,17 @@ describe('Signup', function () {
 
 	before(function (done) {
 		User.model.remove({}, function () {
+			User.register('test1@o2.pl', 'haslo111', function (err) {
+				if (err) {
+					throw err
+				}
+				done();
+			})
+		})
+	});
+
+	after(function (done) {
+		User.model.remove({}, function () {
 			done();
 		})
 	});
@@ -34,6 +45,27 @@ describe('Signup', function () {
 				// logger.info(JSON.stringify(res.body));
 				res.should.have.status(200);
 				res.body.should.have.property('success').eql(true);
+
+				done();
+			})
+	});
+
+	it('given same user email when posting data to the server then should return false', function (done) {
+		var user = {
+			email: 'test1@o2.pl',
+			password: 'haslohaslohasla'
+		}
+
+		chai.request(server)
+			.post('/api/signup')
+			.send(user)
+			.end((err, res) => {
+				if (err) {
+					throw err
+				}
+				// logger.info(JSON.stringify(res.body));
+				res.should.have.status(200);
+				res.body.should.have.property('success').eql(false);
 
 				done();
 			})
